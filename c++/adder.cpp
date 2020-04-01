36f3037ca506af5f9e4dd4661b52ea3c5cd27a5f
acolite-d <dormanjohn68@gmail.com>
2020-04-01 22:25:39 +0000

    Created a simple addition program in C++

---
+++ c++/adder.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int num1, num2;
+	cout << "Welcome to my addition program!" << endl 
+		<< "Please enter two numbers separated by space to add: ";
+	cin >> num1 >> num2;
+
+	cout << "The sum of these two numbers is " << num1 + num2 << endl;
+	return 0;
+}
